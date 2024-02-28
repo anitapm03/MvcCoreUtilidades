9990f53b6e65237d5371b1c8cf04515149b813b8
anitapm03 <apereiramolinero@gmail.com>
2024-02-28 14:21:16 +0100

    Add project files.

---
+++ MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+
+        private IWebHostEnvironment hostEnvironment;
+
+        public UploadFilesController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<IActionResult> SubirFichero()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SubirFichero(IFormFile fichero)
+        {
+            ////aqui trabajaremos con files, es decir, system.IO
+            ////comenzamos almacenando el fichero en una ruta temporal
+
+            //string tempFolder = Path.GetTempPath();
+            //string filename = fichero.FileName;
+
+            //SEGUNDA VERSION
+            string rootFolder =
+                this.hostEnvironment.WebRootPath;
+            string filename = fichero.FileName;
+
+            //necesitamos la ruta fisica para poder escribir el fichero 
+            //la ruta es la combinación de tempfolder y filename 
+            // C:\Documents\Temp\file1.txt
+            //cuando estemos hablando de files (System.IO) para 
+            //acceder a rutas debemos usar path.combine
+            string path = Path.Combine(rootFolder, "uploads", filename);
+
+            //subimos el fichero utilizando stream
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                //mediante IformFile copiamoes el
+                //Contenido del fichero al stream
+                await fichero.CopyToAsync(stream);
+            }
+            ViewData["MENSAJE"] = "fichero subido a " + path;
+            return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperPathProvider.cs
@@ -0,0 +1,40 @@
+﻿namespace MvcCoreUtilidades.Helpers
+{
+    //aqui debemos de tener todas las carpetas que
+    //deseemos que nuestros controllers utilicen
+    public enum Folders { Images = 0, Facturas = 1, Uploads = 2, Temporal = 3 }
+    public class HelperPathProvider
+    {
+        //necesitamos acceder al sistema de archivos del webserver
+        private IWebHostEnvironment hostEnvironment;
+
+        public HelperPathProvider(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public string MapPath(string fileName, Folders folder)
+        {
+            string carpeta = "";
+            if (folder == Folders.Images)
+            {
+                carpeta = "images";
+            }else if (folder == Folders.Facturas)
+            {
+                carpeta = "facturas";
+            }
+            else if(folder == Folders.Uploads)
+            {
+                carpeta = "uploads";
+            }
+            else if (folder == Folders.Temporal)
+            {
+                carpeta = "temp";
+            }
+
+            string rootPath = this.hostEnvironment.WebRootPath;
+            string path = Path.Combine(rootPath, carpeta, fileName);
+            return path;
+        }
+    }
+}
